d592a950aeae8b729a38a22f6db1491465be97bc
1024bees <69404186+1024bees@users.noreply.github.com>
2024-05-09 09:09:05 -0400

    fix tests (#8)
    Co-authored-by: James Connolly <james@silogy.io>

--- pytests/test_graphs_simple.py
+++ pytests/test_graphs_simple.py
@@ -8,7 +8,7 @@ def test_sanity_pygraph():
     test_list = f"{get_git_root()}/test_data/command_lists/cl1.yaml"
     lod = yaml.safe_load(open(test_list))
     commands = [Command.from_dict(obj) for obj in lod]
-    graph = PyGraph.from_command_list(commands)
+    graph = PyGraph.init_commands_only(commands)
     graph.run_all_tests("build")
     graph.run_all_tests("test")
 
--- pytests/test_otl_parser.py
+++ pytests/test_otl_parser.py
@@ -1,5 +1,5 @@
 from pyotl.rc import OtlRC
-from pyotl.otl_muncher import otl_to_command_list
+from pyotl.otl_muncher import parse_otl
 
 
 from pyotl.path_utils import get_git_root
@@ -8,7 +8,5 @@ from pyotl.path_utils import get_git_root
 def test_sanity_otl_parse():
 
     test_list = f"{get_git_root()}/examples/tests_only.otl"
-    commands = otl_to_command_list(
-        test_list=test_list, rc=OtlRC.default(), default_rules_only=True
-    )
+    _, commands = parse_otl(test_list=test_list, default_rules_only=True)
     assert len(commands) == 3, "Didn't parse out every command"
