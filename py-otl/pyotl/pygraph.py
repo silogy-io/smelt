835164372457eef3ab2b000ba73a7db214e74aee
1024bees <69404186+1024bees@users.noreply.github.com>
2024-05-13 20:26:46 -0400

    Interactive2 (#15)
    adding the runloop generator to make things nice for our server
    
    ---------
    
    Co-authored-by: James Connolly <james@silogy.io>

--- py-otl/pyotl/pygraph.py
+++ py-otl/pyotl/pygraph.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List, Optional, Tuple, cast
+from typing import Callable, Dict, Generator, List, Optional, Tuple, cast
 from pyotl.interfaces import Command, OtlTargetType, Target
 from dataclasses import dataclass
 from pyotl.otl_muncher import lower_target_to_command, parse_otl
@@ -65,7 +65,7 @@ class PyGraph:
     controller: PyController
     listener: PySubscriber
     done_tracker = IsDoneSubscriber()
-    retcode_tacker = RetcodeTracker()
+    retcode_tracker = RetcodeTracker()
 
     def runloop(self):
         with OutputConsole() as console:
@@ -75,13 +75,23 @@ class PyGraph:
                 message = maybe_get_message(self.listener, blocking=False)
                 if message:
                     self.done_tracker.process_message(message)
-                    self.retcode_tacker.process_message(message)
+                    self.retcode_tracker.process_message(message)
                     console.process_message(message)
                 if not message:
                     # add a little bit of backoff
                     time.sleep(0.01)
 
-    def console_runloop(self, test_name: str, sink: StdoutSink):
+    def console_runloop(
+        self, test_name: str, sink: StdoutSink
+    ) -> Generator[bool, None, None]:
+        """
+        Generator that will try to consume as many `Event` messages as possible
+
+        All of the stdout from the "test_name" command will be given to the `sink`.
+        By default, sink should just be print
+
+        The yielded value will be
+        """
         stdout_tracker = StdoutPrinter(test_name, sink)
         while True:
             # tbh, this could be async
@@ -89,22 +99,15 @@ class PyGraph:
             message = maybe_get_message(self.listener, blocking=False)
             if message:
                 self.done_tracker.process_message(message)
-                self.retcode_tacker.process_message(message)
+                self.retcode_tracker.process_message(message)
                 stdout_tracker.process_message(message)
             if not message:
                 # add a little bit of backoff
-                if self.done_tracker.is_done:
-                    break
-                time.sleep(0.01)
+                yield self.done_tracker.is_done
 
     def reset(self):
         self.done_tracker.reset()
-        self.retcode_tacker.reset()
-
-    def run_one_test(self, name: str):
-        self.reset()
-        self.controller.run_one_test(name)
-        self.runloop()
+        self.retcode_tracker.reset()
 
     def run_one_test_interactive(self, name: str, sink: StdoutSink = print):
         """
@@ -112,7 +115,10 @@ class PyGraph:
         """
         self.reset()
         self.controller.run_one_test(name)
-        self.console_runloop(name, sink)
+        for is_done in self.console_runloop(name, sink):
+            if is_done:
+                return
+            time.sleep(0.1)
 
     def run_specific_commands(self, commands: List[Command]):
         self.reset()
@@ -134,7 +140,7 @@ class PyGraph:
         if self.otl_targets:
             new_targets = [
                 rerun_callback(self.otl_targets[target], retcode)
-                for target, retcode in self.retcode_tacker.retcode.items()
+                for target, retcode in self.retcode_tracker.retcode_dict.items()
             ]
 
             filtered_targets = [target for target in new_targets if target]
