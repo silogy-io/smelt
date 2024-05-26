e9948c0f0bc30fc83946be633bfab6464ed2c019
1024bees <69404186+1024bees@users.noreply.github.com>
2024-05-26 10:14:34 -0400

    removed unused, add new tests in (#25)
    Co-authored-by: James Connolly <james@silogy.io>

---
+++ pytests/test_invalid_graphs.py
@@ -0,0 +1,24 @@
+from pyotl.pygraph import PyGraph, create_graph, create_graph_with_docker
+from pyotl.path_utils import get_git_root
+from pyotl.interfaces import Command
+
+
+import yaml
+import pytest
+
+from pytests.common import create_command_list_graph
+
+
+def test_missing_dep_graph():
+    with pytest.raises(RuntimeError) as e_info:
+        create_command_list_graph(cl_name="cl_invalid_nodep.yaml")
+
+
+def test_missing_file_dep_graph():
+    with pytest.raises(RuntimeError) as e_info:
+        create_command_list_graph(cl_name="cl_invalid_missing_file_dep.yaml")
+
+
+def test_invalid_graph():
+    with pytest.raises(RuntimeError) as e_info:
+        create_command_list_graph(cl_name="cl_invalid.yaml")
