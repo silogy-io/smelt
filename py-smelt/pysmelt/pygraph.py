1a99563e6f80770d97ce766bc32e04c3abcafa0c
Paul Kim <paul@silogy.io>
2024-07-18 18:02:03 -0700

    Support for setting ulimit and mac address in Docker executor
    Also, ran Black on a few files.

--- py-smelt/pysmelt/pygraph.py
+++ py-smelt/pysmelt/pygraph.py
@@ -1,33 +1,28 @@
-from typing import Callable, Dict, Generator, List, Optional, Tuple, cast
-
-from dataclasses import replace
-import betterproto
-from pysmelt.interfaces import Command, SmeltTargetType, Target
-from dataclasses import dataclass
-from pysmelt.interfaces.paths import SmeltPath
-from pysmelt.smelt_muncher import SmeltUniverse, create_universe, parse_smelt
-from pysmelt.path_utils import relatavize_inp_path
-from pysmelt.pysmelt import PyController, PyEventStream
-from pysmelt.rc import SmeltRcHolder
-
-from pysmelt.subscribers import SmeltSub
-import yaml
 import time
+from dataclasses import dataclass
+from typing import Callable, Generator, List, Optional
 
+import yaml
+from pysmelt.pysmelt import PyController, PyEventStream
 
-
+from pysmelt.interfaces import Command
+from pysmelt.interfaces.paths import SmeltPath
+from pysmelt.proto.smelt_client.commands import (
+    CfgDocker,
+    CfgLocal,
+    ConfigureSmelt,
+    ProfilerCfg,
+    ProfilingSelection,
+)
 from pysmelt.proto.smelt_telemetry import Event
-from pysmelt.proto.smelt_client.commands import CfgDocker, CfgLocal, ConfigureSmelt, ProfilerCfg, ProfilingSelection
-
-
+from pysmelt.rc import SmeltRcHolder
+from pysmelt.smelt_muncher import SmeltUniverse, create_universe
+from pysmelt.subscribers import SmeltSub
 from pysmelt.subscribers.error_handler import SmeltErrorHandler
 from pysmelt.subscribers.invocation_builder import InvocationBuilder
 from pysmelt.subscribers.output_collector import OutputConsole
 from pysmelt.subscribers.retcode import RetcodeTracker
-
-
 from pysmelt.subscribers.stdout import StdoutPrinter, StdoutSink
-
 
 
 def default_cfg() -> ConfigureSmelt:
@@ -38,18 +33,17 @@ def default_cfg() -> ConfigureSmelt:
     rv.smelt_root = rc.smelt_root
     rv.local = CfgLocal()
     rv.prof_cfg = ProfilerCfg()
-    # sample memory 
+    # sample memory
     rv.prof_cfg.prof_type = ProfilingSelection.SIMPLE_PROF
     # sample every 100 ms
     rv.prof_cfg.sampling_period = 100
     return rv
 
 
-
 def maybe_get_message(
     listener: PyEventStream, blocking: bool = False
 ) -> Optional[Event]:
-    try: 
+    try:
         if blocking:
             message = listener.pop_message_blocking()
             event = Event.FromString(message)
@@ -58,35 +52,33 @@ def maybe_get_message(
             if message is None:
                 return None
             event = Event.FromString(message)
-        
+
         return event
-    except RuntimeError as e: 
+    except RuntimeError as e:
         """
-        In expectation, we only return an error if the channel has been closed  
+        In expectation, we only return an error if the channel has been closed
 
         We don't have any paths above this function to handle that error, so we'll handle it here
         """
-        
+
         print(e)
         return None
 
 
-def spin_for_message(
-    listener: PyEventStream, backoff: float = 0.2, time_out: int = 10):
+def spin_for_message(listener: PyEventStream, backoff: float = 0.2, time_out: int = 10):
     """
-    Utility for spinning for a message 
+    Utility for spinning for a message
     """
-    time_passed = 0 
+    time_passed = 0
     while time_passed < time_out:
         message = maybe_get_message(listener, blocking=False)
         if message:
             return message
         else:
-            time.sleep(backoff) 
+            time.sleep(backoff)
             time_passed += backoff
 
     raise RuntimeError(f"Before timeout of {time_out} expired!")
-
 
 
 @dataclass
@@ -99,8 +91,8 @@ class PyGraph:
     """
     Handle to rust -- calls the function defined in the pyo3 bindings
     """
-    
-    retcode_tracker : RetcodeTracker
+
+    retcode_tracker: RetcodeTracker
 
     additional_listeners: List[SmeltSub]
 
@@ -125,7 +117,7 @@ class PyGraph:
         invbuilder.write_invocation_and_junit()
 
     def console_runloop(
-            self, test_name: str, listener: PyEventStream, sink: StdoutSink
+        self, test_name: str, listener: PyEventStream, sink: StdoutSink
     ) -> Generator[bool, None, None]:
         """
         Generator that will try to consume as many `Event` messages as possible
@@ -146,25 +138,26 @@ class PyGraph:
                 stdout_tracker.process_message(message)
                 errhandler.process_message(message)
                 for other_listener in self.additional_listeners:
-                        other_listener.process_message(message)
+                    other_listener.process_message(message)
 
             if not message:
                 # add a little bit of backoff
                 yield listener.is_done()
+
     def get_current_cfg(self) -> ConfigureSmelt:
-        raw_cfg = self.controller.get_current_cfg() 
+        raw_cfg = self.controller.get_current_cfg()
         return ConfigureSmelt.FromString(raw_cfg)
 
     def reset(self):
         pass
-       # self.retcode_tracker.reset()
-        
+
+    # self.retcode_tracker.reset()
 
     def run_one_test_interactive(self, name: str, sink: StdoutSink = print):
         """
         Runs a single test, with a "sink" handle to process all of the stdout for that specific command
 
-        By default, this will just print stdout + stderr to the screen -- it looks like you're running the command interactively 
+        By default, this will just print stdout + stderr to the screen -- it looks like you're running the command interactively
         """
         self.reset()
         listener = self.controller.run_one_test(name)
@@ -185,61 +178,68 @@ class PyGraph:
         self.runloop(listener)
 
     def run_all_commands(self):
-        self.reset() 
+        self.reset()
         toptests = self.universe.top_level_commands
-        valid_commands = [command.name for command in toptests if command.target_type != "rebuild" and command.target_type != "rerun" ]
+        valid_commands = [
+            command.name
+            for command in toptests
+            if command.target_type != "rebuild" and command.target_type != "rerun"
+        ]
         listener = self.controller.run_many_tests(valid_commands)
         self.runloop(listener)
-
 
     def set_commands(self):
         """
         Initializes the list of commands that are visible to the smelt runtime
 
 
-        If the list of commands are malformed -- e.g. syntax error in the yaml, then an error will be thrown 
+        If the list of commands are malformed -- e.g. syntax error in the yaml, then an error will be thrown
         """
         commands = self.universe.all_commands
         commands_as_str = yaml.safe_dump([command.to_dict() for command in commands])
         self.controller.set_graph(commands_as_str)
 
     @classmethod
-    def init(cls, cfg : ConfigureSmelt, universe : SmeltUniverse):
+    def init(cls, cfg: ConfigureSmelt, universe: SmeltUniverse):
         cfg_bytes = bytes(cfg)
         graph = PyController(cfg_bytes)
         rv = cls(
-            universe=universe, controller=graph, retcode_tracker= RetcodeTracker(), additional_listeners=[]
+            universe=universe,
+            controller=graph,
+            retcode_tracker=RetcodeTracker(),
+            additional_listeners=[],
         )
         rv.set_commands()
         return rv
 
-    
     @classmethod
     def init_commands_only(cls, commands: List[Command]):
         cfg = default_cfg()
-        top_path = SmeltPath.from_str('.')
-        universe = SmeltUniverse(top_file=top_path, commands={top_path : commands})
+        top_path = SmeltPath.from_str(".")
+        universe = SmeltUniverse(top_file=top_path, commands={top_path: commands})
         return cls.init(cfg, universe=universe)
 
-def _create_cfg(smelt_test_list: str) -> ConfigureSmelt:
-    command_def_path = SmeltPath.from_str(relatavize_inp_path(SmeltRcHolder.current_rc().smelt_root,smelt_test_list))
+
+def _create_cfg() -> ConfigureSmelt:
     cfg = default_cfg()
     return cfg
 
 
-def create_graph(smelt_test_list: str, cfg_init: Optional[Callable[[ConfigureSmelt],ConfigureSmelt]] = None) -> PyGraph:
-    cfg = _create_cfg(smelt_test_list)
+def create_graph(
+    smelt_test_list: str,
+    cfg_init: Optional[Callable[[ConfigureSmelt], ConfigureSmelt]] = None,
+) -> PyGraph:
+    cfg = _create_cfg()
     if cfg_init:
         cfg = cfg_init(cfg)
     universe = create_universe(SmeltPath.from_str(smelt_test_list))
     rv = PyGraph.init(cfg, universe)
     return rv
 
-def create_graph_with_docker(smelt_test_list: str, docker_img: str) -> PyGraph:
-    def init_docker(cfg: ConfigureSmelt) -> ConfigureSmelt: 
-        cfg.docker = CfgDocker()
-        cfg.docker.image_name = docker_img
-        cfg.docker.additional_mounts = {}
+
+def create_graph_with_docker(smelt_test_list: str, cfg_docker: CfgDocker) -> PyGraph:
+    def init_docker(cfg: ConfigureSmelt) -> ConfigureSmelt:
+        cfg.docker = cfg_docker
         return cfg
-    return create_graph(smelt_test_list,init_docker)
-    
+
+    return create_graph(smelt_test_list, init_docker)
