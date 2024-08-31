3d5cae4574b38ccc9505dd50b21316681b4c2328
James Connolly <james@silogy.io>
2024-08-30 17:04:59 -0700

    woops, fix

--- test_utils/exec_slurm_tests.sh
+++ test_utils/exec_slurm_tests.sh
@@ -1,2 +1,2 @@
 docker build --platform linux/amd64  --build-arg SMELT_VERSION=$(bash test_utils/get_version.sh) -t smelt_dev . -f test_utils/Dockerfile.slurm_dev
-docker run --hostname slurmctl --platform linux/amd64 -it smeltdev
+docker run --hostname slurmctl --platform linux/amd64 smeltdev
