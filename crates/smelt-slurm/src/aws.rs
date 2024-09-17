7ed98156fe8fab51ffb6c3d7426723832c514e3d
James Connolly <james@silogy.io>
2024-09-16 18:20:29 -0700

    update

--- crates/smelt-slurm/src/aws.rs
+++ crates/smelt-slurm/src/aws.rs
@@ -1,139 +1,131 @@
+use std::path::{Path, PathBuf};
 
+use anyhow;
+use aws_config::BehaviorVersion;
+use aws_sdk_s3 as s3;
 
+use aws_credential_types::{provider::ProvideCredentials, Credentials};
 
-//In bytes, minimum chunk size of 5MB. Increase CHUNK_SIZE to send larger chunks.
-//const CHUNK_SIZE: u64 = 1024 * 1024 * 5;
-//const MAX_CHUNKS: u64 = 10000;
-//
-//async fn run_example() -> Result<(), S3ExampleError> {
-//    let shared_config = aws_config::load_from_env().await;
-//    let client = S3Client::new(&shared_config);
-//
-//    let region_provider = RegionProviderChain::first_try(Region::new("us-west-2"));
-//    let region = region_provider.region().await.unwrap();
-//    s3_code_examples::create_bucket(&client, &bucket_name, &region).await?;
-//
-//    let key = "sample.txt".to_string();
-//    // Create a multipart upload. Use UploadPart and CompleteMultipartUpload to
-//    // upload the file.
-//    let multipart_upload_res: CreateMultipartUploadOutput = client
-//        .create_multipart_upload()
-//        .bucket(&bucket_name)
-//        .key(&key)
-//        .send()
-//        .await?;
-//
-//    let upload_id = multipart_upload_res.upload_id().ok_or(S3ExampleError::new(
-//        "Missing upload_id after CreateMultipartUpload",
-//    ))?;
-//
-//    //Create a file of random characters for the upload.
-//    let mut file = File::create(&key).expect("Could not create sample file.");
-//    // Loop until the file is 5 chunks.
-//    while file.metadata().unwrap().len() <= CHUNK_SIZE * 4 {
-//        let rand_string: String = thread_rng()
-//            .sample_iter(&Alphanumeric)
-//            .take(256)
-//            .map(char::from)
-//            .collect();
-//        let return_string: String = "\n".to_string();
-//        file.write_all(rand_string.as_ref())
-//            .expect("Error writing to file.");
-//        file.write_all(return_string.as_ref())
-//            .expect("Error writing to file.");
-//    }
-//
-//    let path = Path::new(&key);
-//    let file_size = tokio::fs::metadata(path)
-//        .await
-//        .expect("it exists I swear")
-//        .len();
-//
-//    let mut chunk_count = (file_size / CHUNK_SIZE) + 1;
-//    let mut size_of_last_chunk = file_size % CHUNK_SIZE;
-//    if size_of_last_chunk == 0 {
-//        size_of_last_chunk = CHUNK_SIZE;
-//        chunk_count -= 1;
-//    }
-//
-//    if file_size == 0 {
-//        return Err(S3ExampleError::new("Bad file size."));
-//    }
-//    if chunk_count > MAX_CHUNKS {
-//        return Err(S3ExampleError::new(
-//            "Too many chunks! Try increasing your chunk size.",
-//        ));
-//    }
-//
-//    let mut upload_parts: Vec<aws_sdk_s3::types::CompletedPart> = Vec::new();
-//
-//    for chunk_index in 0..chunk_count {
-//        let this_chunk = if chunk_count - 1 == chunk_index {
-//            size_of_last_chunk
-//        } else {
-//            CHUNK_SIZE
-//        };
-//        let stream = ByteStream::read_from()
-//            .path(path)
-//            .offset(chunk_index * CHUNK_SIZE)
-//            .length(Length::Exact(this_chunk))
-//            .build()
-//            .await
-//            .unwrap();
-//
-//        // Chunk index needs to start at 0, but part numbers start at 1.
-//        let part_number = (chunk_index as i32) + 1;
-//        let upload_part_res = client
-//            .upload_part()
-//            .key(&key)
-//            .bucket(&bucket_name)
-//            .upload_id(upload_id)
-//            .body(stream)
-//            .part_number(part_number)
-//            .send()
-//            .await?;
-//
-//        upload_parts.push(
-//            CompletedPart::builder()
-//                .e_tag(upload_part_res.e_tag.unwrap_or_default())
-//                .part_number(part_number)
-//                .build(),
-//        );
-//    }
-//
-//    // upload_parts: Vec<aws_sdk_s3::types::CompletedPart>
-//    let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
-//        .set_parts(Some(upload_parts))
-//        .build();
-//
-//    let _complete_multipart_upload_res = client
-//        .complete_multipart_upload()
-//        .bucket(&bucket_name)
-//        .key(&key)
-//        .multipart_upload(completed_multipart_upload)
-//        .upload_id(upload_id)
-//        .send()
-//        .await?;
-//
-//    let data: GetObjectOutput =
-//        s3_code_examples::download_object(&client, &bucket_name, &key).await?;
-//    let data_length: u64 = data
-//        .content_length()
-//        .unwrap_or_default()
-//        .try_into()
-//        .unwrap();
-//    if file.metadata().unwrap().len() == data_length {
-//        println!("Data lengths match.");
-//    } else {
-//        println!("The data was not the same size!");
-//    }
-//
-//    s3_code_examples::clear_bucket(&client, &bucket_name)
-//        .await
-//        .expect("Error emptying bucket.");
-//    s3_code_examples::delete_bucket(&client, &bucket_name)
-//        .await
-//        .expect("Error deleting bucket.");
-//
-//    Ok(())
-//}
+use aws_smithy_types::byte_stream::{ByteStream, Length};
+use s3::{
+    operation::create_multipart_upload::CreateMultipartUploadOutput,
+    types::{CompletedMultipartUpload, CompletedPart},
+};
+use tokio::io::AsyncWriteExt;
+
+const AWS_REGION: &str = "us-west-1";
+
+// these constants are arbitrarily chosen, tbh
+// 1mb chunk size
+const CHUNK_SIZE: u64 = 1024 * 1024 * 1;
+// 10gb max artifact size
+const MAX_CHUNKS: u64 = 10000;
+
+pub async fn create_s3_client(key_id: &str, key: &str) -> Result<s3::Client, s3::Error> {
+    let creds = Credentials::new(key_id, key, None, None, "smelt-worker");
+
+    // Look man, i dont know why credentials_provider has a static trait bound
+    // whatever works
+    // but
+    // we are hardcoding support for only us-west-1 because of it
+    //
+    // amz, you make me sin
+    //
+    // and for what?
+
+    let shared_config = aws_config::defaults(BehaviorVersion::v2024_03_28())
+        .region(AWS_REGION)
+        .credentials_provider(creds)
+        .load()
+        .await;
+    let client = s3::Client::new(&shared_config);
+    Ok(client)
+}
+
+pub async fn upload_file(
+    client: s3::Client,
+    bucket_name: &str,
+    key: &str,
+    file_path: PathBuf,
+) -> anyhow::Result<()> {
+    let multipart_upload_res: CreateMultipartUploadOutput = client
+        .create_multipart_upload()
+        .bucket(bucket_name)
+        .key(key)
+        .send()
+        .await?;
+
+    let upload_id = multipart_upload_res.upload_id().ok_or(anyhow::anyhow!(
+        "Missing upload_id after CreateMultipartUpload",
+    ))?;
+
+    let file_size = tokio::fs::metadata(&file_path)
+        .await
+        .expect("it exists I swear")
+        .len();
+
+    let mut chunk_count = (file_size / CHUNK_SIZE) + 1;
+    let mut size_of_last_chunk = file_size % CHUNK_SIZE;
+    if size_of_last_chunk == 0 {
+        size_of_last_chunk = CHUNK_SIZE;
+        chunk_count -= 1;
+    }
+
+    if file_size == 0 {
+        anyhow::bail!("Bad file size.")
+    }
+    if chunk_count > MAX_CHUNKS {
+        anyhow::bail!("Too many chunks! Try increasing your chunk size.")
+    }
+
+    let mut upload_parts: Vec<aws_sdk_s3::types::CompletedPart> = Vec::new();
+
+    for chunk_index in 0..chunk_count {
+        let this_chunk = if chunk_count - 1 == chunk_index {
+            size_of_last_chunk
+        } else {
+            CHUNK_SIZE
+        };
+        let stream = ByteStream::read_from()
+            .path(file_path.as_path())
+            .offset(chunk_index * CHUNK_SIZE)
+            .length(Length::Exact(this_chunk))
+            .build()
+            .await
+            .unwrap();
+
+        // Chunk index needs to start at 0, but part numbers start at 1.
+        let part_number = (chunk_index as i32) + 1;
+        let upload_part_res = client
+            .upload_part()
+            .key(key)
+            .bucket(bucket_name)
+            .upload_id(upload_id)
+            .body(stream)
+            .part_number(part_number)
+            .send()
+            .await?;
+
+        upload_parts.push(
+            CompletedPart::builder()
+                .e_tag(upload_part_res.e_tag.unwrap_or_default())
+                .part_number(part_number)
+                .build(),
+        );
+    }
+
+    // upload_parts: Vec<aws_sdk_s3::types::CompletedPart>
+    let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
+        .set_parts(Some(upload_parts))
+        .build();
+
+    let _complete_multipart_upload_res = client
+        .complete_multipart_upload()
+        .bucket(bucket_name)
+        .key(key)
+        .multipart_upload(completed_multipart_upload)
+        .upload_id(upload_id)
+        .send()
+        .await?;
+    Ok(())
+}
--- slurm-pytests/test_slurm.py
+++ slurm-pytests/test_slurm.py
@@ -59,13 +59,13 @@ def test_sealed_slurm():
     test_list = f"test_data/smelt_files/simple_graph.smelt.yaml"
     img = "test_sealed_slurm_img"
 
-    create_sealed("sealed_example", img, test_list)
+    create_sealed("smelt_dev", img, test_list)
 
     def init_slurm(cfg: ConfigureSmelt) -> ConfigureSmelt:
         cfg.test_only = True
         cfg.slurm = CfgSlurm()
         cfg.slurm.dockerws = DockerWorkspace(
-            container_name=img, workspace_smelt_root="/opt"
+            container_name=img, workspace_smelt_root="/src"
         )
         return cfg
 
