ed479483966115d39e1ac164cae79a92c3b48508
James Connolly <james@silogy.io>
2024-09-24 23:12:13 -0700

    sanity

--- crates/smelt-graph/src/executor/slurm.rs
+++ crates/smelt-graph/src/executor/slurm.rs
@@ -93,15 +93,17 @@ pub fn init_proxy(port: u16) -> u16 {
             });
         });
 
+        let local_proxy = LazyLock::new(|| RwLock::new(None));
+
         {
-            *MAYBE_PROXY.write().unwrap() = Some(ProxyState {
+            *local_proxy.write().unwrap() = Some(ProxyState {
                 servers,
                 jh: handle,
                 port,
             });
         }
         tracing::info!("successfully wrote?");
-        tracing::info!("reading val, the val is {:?}", MAYBE_PROXY.read());
+        tracing::info!("reading val, the val is {:?}", local_proxy.read());
 
         bound_port
     }
