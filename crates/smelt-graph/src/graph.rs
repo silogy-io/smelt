0aa89ca94ef33592fea3bf7fbe08fe01f5cbcac3
James Connolly <james@silogy.io>
2024-09-30 01:07:49 -0700

    prepare logic improve

--- crates/smelt-graph/src/graph.rs
+++ crates/smelt-graph/src/graph.rs
@@ -169,6 +169,7 @@ impl Key for CommandRef {
         let val: Vec<Self::Value> = future::join_all(futs).await.into_iter().collect();
 
         if prepare_only && self.0.target_type.test_only_valid() {
+            tracing::info!("preparing!");
             let command = self.0.as_ref();
             let root = ctx.global_data().get_smelt_root();
             let cfg = ctx.global_data().get_smelt_cfg();
@@ -182,28 +183,34 @@ impl Key for CommandRef {
                     )
                 });
 
-            if let Some(configure_smelt::InitExecutor::Slurm(CfgSlurm {
-                sealed_workspace:
-                    Some(SealedWorkspace::Dockerws(DockerWorkspace {
-                        workspace_smelt_root,
-                        ..
-                    })),
-                ..
-            })) = &cfg.init_executor
-            {
-                let command_working_dir = command.default_target_root(root)?;
-                let _ = prepare_artifact_file(
-                    command,
-                    workspace_smelt_root.to_string(),
-                    command_working_dir.as_path(),
-                )
-                .await
-                .inspect_err(|err| {
-                    tracing::error!(
-                        "Creating the artifact json file failed while creating the workspace with err {err}"
-                    )
-                });
-            }
+            let workspace_smelt_root =
+                if let Some(configure_smelt::InitExecutor::Slurm(CfgSlurm {
+                    sealed_workspace:
+                        Some(SealedWorkspace::Dockerws(DockerWorkspace {
+                            workspace_smelt_root,
+                            ..
+                        })),
+                    ..
+                })) = &cfg.init_executor
+                {
+                    workspace_smelt_root.to_string()
+                } else {
+                    root.to_string_lossy().to_string()
+                };
+
+            let command_working_dir = command.default_target_root(root)?;
+            let _ = prepare_artifact_file(
+                command,
+                workspace_smelt_root.to_string(),
+                command_working_dir.as_path(),
+            )
+            .await
+            .inspect_err(|err| {
+                tracing::error!(
+        "Creating the artifact json file failed while creating the workspace with err {err}"
+    )
+            });
+
             return Ok(Arc::new(ExecutedTestResult::Skipped));
         }
 
